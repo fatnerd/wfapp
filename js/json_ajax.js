0d8af08f4cc4b803d4b486775ca2208a15f5fc33
fatnerd <l.lllkj@163.com>
2014-01-27 14:16:09 +0800

    First try of Ajax System well done

--- js/json_ajax.js
+++ js/json_ajax.js
@@ -1,39 +1,41 @@
+$.ajaxSetup({cache:true,async:true});
 function fillDetail(stationID) {
-	req = new XMLHttpRequest();
-	req.open("GET", "http://218.93.33.59:85/map/wfmap/ibikeinterface.asp?id=" + stationID, /*async*/true);
-	req.onreadystatechange = detailHandler;
-	req.send(/*no params*/null);
-	showload("detload");
-}
-function detailHandler(){
-	if (req.readyState == 4 /*complete*/) {
-       var detid = document.getElementById('detailid').innerHTML;
-	   var detname = document.getElementById('detailname').innerHTML;
-	   var detaddr = document.getElementById('detailadd').innerHTML;
-	   var detcapa = document.getElementById('detailcapacity').innerHTML;
+	showload("detload"); 
+	detnow=stationID;
+	$.getScript("http://218.93.33.59:85/map/wfmap/ibikeinterface.asp?id=" + stationID,function() {
 	   var detdist = document.getElementById('detaildistance').innerHTML;
-	   var detcoloravail = document.getElementById('detailli1');
-	   var detcolorleft = document.getElementById('detailli2');
-	   var detavail = document.getElementById('detailavail').innerHTML;
-	   var detleft = document.getElementById('detailleft').innerHTML;
-	   var detlat = document.getElementById('detaillat').value;
-	   var detlng = document.getElementById('detaillng').value;
-       var singlebike = eval('(' + removeHeader(req.responseText) + ')');
-       detid = singlebike.station[0].id;
-	   detname = singlebike.station[0].name;
-	   detlat = singlebike.station[0].lat;
-	   detlng = singlebike.station[0].lng;
-	   detcapa = singlebike.station[0].capacity;
-	   detaddr = singlebike.station[0].address;
+       document.getElementById('detailid').innerHTML = isinglebike.station[0].id;
+	   document.getElementById('detailname').innerHTML = isinglebike.station[0].name;
+	   document.getElementById('detaillat').value = isinglebike.station[0].lat;
+	   document.getElementById('detaillng').value = isinglebike.station[0].lng;
+	   document.getElementById('detailcapacity').innerHTML = isinglebike.station[0].capacity;
+	   document.getElementById('detailadd').innerHTML = isinglebike.station[0].address;
 	   //Bike Number Process Begin
-	   detavail = singlebike.station.availBike;
-	   detleft = detcapa - detavail;
-	   if (detavail <= 2) {detcoloravail.setAttribute("data-theme", "c");}
-	   else if ((detavail <= 5) && (detavail > 2)) {detcoloravail.setAttribute("data-theme", "f");}
-	   else { detcoloravail.setAttribute("data-theme", "d");} 
-	   if (detleft <= 2) {detcolorleft.setAttribute("data-theme", "c");}
-	   else if ((detleft <= 5) && (detleft > 2)) {detcolorleft.setAttribute("data-theme", "f");} 
-	   else { detcolorleft.setAttribute("data-theme", "d");}
-	   hideload("detload"); 
-   }
+	   document.getElementById('detailavail').innerHTML = isinglebike.station[0].availBike;
+	   var bikeleft = isinglebike.station[0].capacity - isinglebike.station[0].availBike
+	   document.getElementById('detailleft').innerHTML = bikeleft;
+	   document.getElementById('detailli1').removeAttribute("class");
+	   document.getElementById('detailli2').removeAttribute("class");
+	   if (isinglebike.station[0].availBike <= 3) {
+		   document.getElementById('detailli1').setAttribute("data-theme", "c");
+		   }
+	   else if ((isinglebike.station[0].availBike <= 6) && (isinglebike.station[0].availBike > 3)) {
+		   document.getElementById('detailli1').setAttribute("data-theme", "f");
+		   }
+	   else { 
+	   document.getElementById('detailli1').setAttribute("data-theme", "d");
+	   } 
+	   if (bikeleft <= 3) {
+		   document.getElementById('detailli2').setAttribute("data-theme", "c");
+		   }
+	   else if ((bikeleft <= 6) && (bikeleft > 3)) {
+		   document.getElementById('detailli2').setAttribute("data-theme", "f");
+		   } 
+	   else { document.getElementById('detailli2').setAttribute("data-theme", "d");}
+	   $('#detailnumlist').listview('refresh');
+	   hideload("detload");
+   });
+}
+function refreshDetail(){
+	fillDetail(detnow);
 }
