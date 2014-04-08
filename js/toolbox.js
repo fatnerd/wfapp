7ee9a3e55decf6b98d77ec9815118205bbe2516a
jsw <l.lllkj@163.com>
2014-04-08 13:28:08 +0800

    A LOT MORE CHANGES

--- js/toolbox.js
+++ js/toolbox.js
@@ -1,3 +1,96 @@
+function bubbleSort(theArray)
+{
+  var i, j;
+  for (i = theArray.length - 1; i >= 0; i--)
+  {
+    for (j = 0; j <= i; j++)
+    {
+      if (theArray[j+1] < theArray[j])
+      {
+        var temp = theArray[j];
+        theArray[j] = theArray[j+1];
+        theArray[j+1] = temp;
+      }
+    }
+  }
+  return theArray;
+}
+function bubbleSort2(theArray)
+{
+  var i, j;
+  for (i = theArray.length - 1; i >= 0; i--)
+  {
+    for (j = 0; j <= i; j++)
+    {
+      if (theArray[j+1].distance < theArray[j].distance)
+      {
+        var temp = theArray[j];
+        theArray[j] = theArray[j+1];
+        theArray[j+1] = temp;
+      }
+    }
+  }
+  return theArray;
+}
+
+function Rad(d){
+       return d * Math.PI / 180.0;
+    }
+function GetDistance(lat1,lng1,lat2,lng2){
+        var radLat1 = Rad(lat1); //mylat
+        var radLat2 = Rad(lat2);
+        var a = radLat1 - radLat2;
+        var  b = Rad(lng1) - Rad(lng2); //mylng
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
+        Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
+        s = s *6378.137 ;
+        s = Math.round(s * 10000) / 10000; 
+		s = s * 1000; //to Meter
+        //s=s.toFixed(4);
+        return s;
+}
+function distFiller(mlat,mlng,station,callback) {
+	for (var i in station){
+		station[i].distance=GetDistance(mlat,mlng,station[i].lat,station[i].lng);
+	}
+	var station2 = bubbleSort2(station);
+	callback(station2);
+}/*  WFiBike_app: Weifang Public Bicycle HTML5(Cross-Platform) App
+    Copyright (C) 2014  js,w
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Contact: l.lllkj@163.com  with a header contains "[pj_WFiBike_APP]"
+*/
+function Navitodetail(){
+	var lat=document.getElementById('detaillat').value;
+	var lng=document.getElementById('detaillng').value;
+	var name=document.getElementById("detailname").innerHTML;
+	if (useBMap){
+    	window.open("baidumap://map/marker?location="+lat+","+lng+"&title=目标站点&content="+name+"&coord_type=gcj02&src=jsw|wfibikeapp");
+	} else if (useAMap){
+		window.open("iosamap://navi?sourceApplication=safari&backScheme=http&dlat="+lat+"&dlon="+lng+"&dname="+name+"&dev=0&t=4");
+	} else {
+		alert("您没有设置导航应用！");
+		alert("debug comment:\nType to URL to set AMap: javascript:useAMap=true; \n likely on BMap \n Need Application");
+	}
+}
+function getLocation(callback,unavailcallback){
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(callback,unavailcallback);
+  }
+  else{console.log("Geolocation is not supported by this browser.");unavailcallback();}
+}
 /*  WFiBike_app: Weifang Public Bicycle HTML5(Cross-Platform) App
     Copyright (C) 2014  js,w
 
---
+++ variant.js
@@ -0,0 +1,2 @@
+//Data Variant Set For ibike668 System.
+var loc_interface = "http://218.93.33.59:85/map/wfmap/ibikeinterface.asp" //ibikeinterface location
