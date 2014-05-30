c95e8d87b0224b9848a3786f4c42b604575cb0e3
jsw <l.lllkj@163.com>
2014-05-30 17:32:59 +0800

    lbs initalized

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,6 +1,15 @@
 function getLocation(callback,unavailcallback){
   if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(callback,unavailcallback);
+    navigator.geolocation.getCurrentPosition(function(){
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+		$.getJSON("http://api.map.baidu.com/ag/coord/convert?from=0&to=2&x="+lng+"&y="+lat+"&callback=?",function(data){
+			var newlng=window.atob(data.x);
+			var newlat=window.atob(data.y);
+			callback({"position":{"coords":{"latitude":newlat,"longitude":newlng}}});
+			});
+		}
+	,unavailcallback);
   }
   else{console.log("Geolocation is not supported by this browser.");unavailcallback();}
 }
